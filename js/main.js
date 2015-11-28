4840cf4e7d0367a8c859efb7abfeaec0c3b48bce
Tim Mondonedo <mondonedot@Tims-MacBook-Pro.local>
2015-11-28 10:53:14 -0500

    Built basic interface.

--- js/main.js
+++ js/main.js
@@ -1,31 +1,31 @@
 $(document).ready(function(){
 
-	var user;
-
-	function User(username, password) {
+	var User = function(username, password, summoner) {
 		this.username = username;
 		this.password = password;
+		this.summoner = summoner;
 	}
+
+	var user = User('Not logged in', '');
+
     $('.app').hide();
     $("#login").click(function(){ 
     	console.log("login clicked");
         var username = $("#loginUsername").val();
         var password = $("#loginPassword").val();
-        console.log(username);
-        console.log(password);
         $.post("../php/app.php", {action: "login", username: username, password: password}, function(data)  {
-        	console.log("dick");
-        	//alert(data);
-	        if (data === "True") {
-	        	$('.loginsignup').hide();
-	        	$('.app').show();
+        	console.log("logging in");
+	        if (data == "True") {
 
-	        	user = new User(username, password);
+				//get summoner name for the corresponding user
+				$.post("../php/app.php", {action: "getSummoner", username: username}, function(data){	
+					summoner = data;
+					showApp(username, password, summoner);
+				});
 	        }
 	        else if(data == "False"){
-	        	console.log("gg");
+				$('#login-msg').text('Invalid username or password');
 	        }
-
     	});
 
     });
@@ -33,41 +33,69 @@ $(document).ready(function(){
     $("#signup").click(function() {
     	var username = $("#signUsername").val();
         var password = $("#signPassword").val();
+		var summoner = $("#signSummoner").val();
 
-        $.post("../php/app.php", {action: "signup", username: username, password: password}, function(data) {
-        	$('.loginsignup').hide();
-	        $('.app').show();
-
-	        user = new User(username, password);
+        $.post("../php/app.php", {action: "signup", username: username, password: password, summoner: summoner}, function(data) {
+			if (data == "User already exists") {
+				$('#signup-msg').text('Username already exists');
+			} else if (data == "Success") {
+				showApp(username, password, summoner);
+			}
         });
 
     });
 
+	//Shows the app once the user signs up or logs in.
+	function showApp(username, password, summoner) {	
+	    user = new User(username, password, summoner);
+		$('.loginsignup').hide();
+		$('.app').show();
+
+		//Display the username and summoner name
+		$('#login-name').text(user.username);
+		$('#summoner-name').text(user.summoner);
+
+		//Display the user's favorite champions
+		displayFavs(username);
+	}
+
+	function displayFavs(username) {
+        $.post("../php/app.php", {action: "getFavs", username: username}, function(data) {
+			if (data == "No favorites") {
+				//Do nothing
+			} else {
+				$('#no-fav-champs').hide();
+				$('#fav-champ-list').append(data);
+
+				//Bind the delete champ button for each champ
+				$('.deleteChamp').click(function() {
+					var champ = $(this).attr('value');
+					console.log(champ);
+					$.post("../php/app.php", {action: "deleteChamp", username: user.username, champ: champ}, function(data) {
+					if(data === "fail"){
+						//do nothing
+					}
+					else
+						$('#' + champ).remove()
+					});
+				});
+			}
+        });
+	}
+
     $('#favChamp').click(function() {
     	var fav = $('#inputFav').val();
-    	//alert(fav);
     	$.post("../php/app.php", {action: "favChamp", username: user.username, favoriteChamp: fav}, function(data) {
-    		//alert(data);
     		$('#inputFav').val('');
         	if(data === "exists"){
         		//do nothing
         	}
-        	else
-        		$('.app').append("<p id="+fav+">"+fav+"</p>")
+        	else {
+				//reload the favorites
+				$('#fav-champ-list').empty();
+				displayFavs(user.username);
+			}
         });
     });
 
-    $('#deleteChamp').click(function() {
-    	var champ = $('#inputDelete').val();
-    	//alert(fav);
-    	$.post("../php/app.php", {action: "deleteChamp", username: user.username, champ: champ}, function(data) {
-    		//alert(data);
-    		$('#inputDelete').val('');
-        	if(data === "fail"){
-        		//do nothing
-        	}
-        	else
-        		$('#' + champ).remove()
-        });
-    });
 });
--- php/app.php
+++ php/app.php
@@ -13,13 +13,13 @@ if (mysqli_connect_error()){
 	die("Database failed: " . mysqli_connect_error());
 }
 
-if($_POST['action'] === "login")  {
-//Check that username and pword exist in DB
-$username = $_POST["username"];
-$password = $_POST["password"];
+if($_POST['action'] === "login") {
+	//Check that username and pword exist in DB
+	$username = $_POST["username"];
+	$password = $_POST["password"];
 
-$sql = "SELECT * FROM users WHERE name = \"$username\" AND password = \"$password\"";
-$result = mysqli_query($conn, $sql);
+	$sql = "SELECT * FROM users WHERE name = \"$username\" AND password = \"$password\"";
+	$result = mysqli_query($conn, $sql);
 
 	if (mysqli_num_rows($result) > 0){
 			echo "True";
@@ -33,19 +33,26 @@ $result = mysqli_query($conn, $sql);
 	}
 }
 
-//TODO validate unique username penis
-
 if($_POST['action'] === "signup") {
 	//Manipulate some data in the db
 	$username = $_POST["username"];
 	$password = $_POST["password"];
+	$summoner = $_POST["summoner"];
 
-	$sql = "INSERT INTO users VALUES (\"$username\", \"$password\")";
-	//echo $sql;
+	$sql = "SELECT * FROM users WHERE name = \"$username\"";
 	$result = mysqli_query($conn, $sql);
-
-
-
+	if (mysqli_num_rows($result) > 0){ //If the user exists then don't add it
+		echo "User already exists";
+		mysqli_close($conn);
+		return;
+	}
+	else{
+		echo "Success";
+		$sql = "INSERT INTO users VALUES (\"$username\", \"$password\", \"$summoner\")";
+		$result = mysqli_query($conn, $sql);
+		mysqli_close($conn);
+		return;
+	}
 }
 
 
@@ -80,3 +87,42 @@ if($_POST['action'] === "deleteChamp") {
 	}
 }
 
+if($_POST['action'] === "getSummoner") {
+	$username = $_POST["username"];
+
+	$sql = "SELECT * FROM users WHERE name = \"$username\"";
+	$result = mysqli_query($conn, $sql);
+
+	if (mysqli_num_rows($result) > 0){
+		while($row = mysqli_fetch_assoc($result)) {
+			echo $row["summoner_name"];
+		}
+		mysqli_close($conn);
+		return;
+	}
+	else{
+		echo "False";
+		mysqli_close($conn);
+		return;
+	}
+}
+
+if($_POST['action'] === "getFavs") {
+	$username = $_POST["username"];
+
+	$sql = "SELECT * FROM favoritechampions WHERE name = \"$username\"";
+	$result = mysqli_query($conn, $sql);
+
+	if (mysqli_num_rows($result) > 0){
+		while($row = mysqli_fetch_assoc($result)) {
+			echo "<li id=\"" . $row["champion"] . "\">" . $row["champion"] . "<button class=\"deleteChamp\" value=\"" . $row["champion"] . "\" style=\"color:red; background-color:Transparent; border: none\">X</button></li>";
+		}
+		mysqli_close($conn);
+		return;
+	}
+	else{
+		echo "No favorites";
+		mysqli_close($conn);
+		return;
+	}
+}
